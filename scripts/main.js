f32b019625ca621fbc9866a3890dad7eab5d2dcf
sgrund14 <sgrund@oberlin.edu>
2017-01-11 21:44:34 -0500

    working resume/prism buttons

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,27 @@ $(function () {
     $('#info-button').on('click', movePage(2));
     $('#work-button').on('click', movePage(3));
     $('#contact-button').on('click', movePage(4));
+    
+    $('.prism').on('mouseenter', prismColorChange);
+    $('.prism').on('mouseleave', prismReset)
 });
+
+function prismColorChange(){
+    $('.P').addClass('p-hover');
+    $('.R').addClass('r-hover');
+    $('.I').addClass('i-hover');
+    $('.S').addClass('s-hover');
+    $('.M').addClass('m-hover');
+}
+
+function prismReset(){
+    $('.P').removeClass('p-hover');
+    $('.R').removeClass('r-hover');
+    $('.I').removeClass('i-hover');
+    $('.S').removeClass('s-hover');
+    $('.M').removeClass('m-hover');
+}
+
 
 function inflate(index) {
     var $currentPage = $('.on');
