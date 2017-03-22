96a6ccb1ab3f3ea97044a0537042897012925ad9
sgrund14 <sgrund@oberlin.edu>
2017-03-22 16:10:57 -0400

    updated resume, better comments

--- scripts/runanim.js
+++ scripts/runanim.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    // array containing top canvas, bottom canvas
     var mypapers = [];
     var colors = {
         yellow: 'yellow',
@@ -11,6 +12,8 @@ window.onload = function () {
         lightgrey: 'lightgrey'
     };
 
+    // runs animation. id refers to which canvas paper runs on: 0 for top, 1 for bottom.
+    // canvasElement is the canvas to draw on
     var anim = function (id, canvasElement) {
         mypapers[id] = new paper.PaperScope();
         paper = mypapers[id];
@@ -19,22 +22,24 @@ window.onload = function () {
         var count;
         var speed = 8;
 
+        // more circles on bottom canvas, less on top.
         id === 0 ?
             count = 20 : count = 50;
         var path;
 
         var createCircle = function () {
-            // this function is called on resize, so we have to re-fetch the scope!
 
-            // Create a symbol, which we will use to place instances of later:
+            // intialize original circle object, which will create symbols of later
             path = new paper.Path.Circle({
                 center: [0, 0],
                 radius: 10,
                 fillColor: 'yellow'
             });
 
+            // initialize circle colors: yellow for top canvas, grey for bottom
             id === 0 ? path.fillColor = colors.yellow : path.fillColor = colors.lightgrey;
 
+            // Create a symbol, which we will use to place instances of later:
             var symbol = new paper.Symbol(path);
 
             // Place the instances of the symbol:
@@ -49,6 +54,7 @@ window.onload = function () {
 
                 placedSymbol.setPosition(center);
 
+                // larger circles on top canvas, smaller circles on bottom
                 if (id === 0) {
                     placedSymbol.scale(paper.view.size.width / 300 * i / count);
                 } else {
@@ -58,14 +64,17 @@ window.onload = function () {
         }
         createCircle();
 
+        // function to change circle colors on screen change
         var colorChange = function (color) {
             fade(path.fillColor, color, 125);
         }
 
-        function lerp(a, b, u) {
-            return (1 - u) * a + u * b;
+        // helper function for smooth color transition
+        function lerp(val1, val2, rate) {
+            return (1 - rate) * val1 + rate * val2;
         };
 
+        // smoothly transition from start color to end color over duration
         function fade(start, end, duration) {
             var endColor = new paper.Color(end);
 
@@ -95,8 +104,8 @@ window.onload = function () {
                 for (var i = 0; i < count; i++) {
                     var item = mypapers[id].project.activeLayer.children[i];
 
-                    // Move the item 1/20th of its width to the right. This way
-                    // larger circles move faster than smaller circles:     item.position.x += item.bounds.width / 200;
+                    // move circles according to size, speed constant. 
+                    // use sin and cos functions to give movement a less static feel
                     if (id === 0) {
                         item.position.x += Math.sin(item.bounds.width) / speed;
                         item.position.y += Math.cos(item.bounds.height) / speed;
@@ -104,6 +113,8 @@ window.onload = function () {
                         item.position.x -= Math.sin(item.bounds.width) / speed;
                         item.position.y -= Math.cos(item.bounds.height) / speed;
                     }
+
+                    // wrap screen edges
                     if (item.bounds.left > mypapers[id].view.size.width) {
                         item.position.x -= mypapers[id].view.size.width + item.bounds.width
                     } else if (item.bounds.top > mypapers[id].view.size.height) {
@@ -124,14 +135,15 @@ window.onload = function () {
         };
     };
 
+    // start animations for each canvas
     var topAnim = anim(0, $("#canvas")[0]);
     var bottomAnim = anim(1, $("#canvas2")[0]);
 
     mypapers[0].view.onFrame = topAnim.update(0);
     mypapers[1].view.onFrame = bottomAnim.update(1);
 
-
-
+    // use mutation observer to detect HTML changes
+    // checks when page buttons activate/deactivate, change circle colors accordingly 
     var observer = new MutationObserver(function (mutations) {
         if ($('#info-button').hasClass('on')) {
             topAnim.colorChange(colors.lightblue);
@@ -154,7 +166,7 @@ window.onload = function () {
     };
 
     // Node, config
-    // In this case we'll listen to all changes to body and child nodes
+    // Listen to navigation buttons
     var infoNode = document.getElementById('info-button');
     var workNode = document.getElementById('work-button');
     var contactNode = document.getElementById('contact-button');
